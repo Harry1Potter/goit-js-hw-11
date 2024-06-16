b0bbb8f4fb39d2a3fb8adf6eb1a9f41086c9a3f9
Max <maks34589@gmail.com>
2024-06-16 12:10:27 +0300

    Your descriptive message here

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { fetchImages } from './js/pixabay-api.js';
+import { displayImages } from './js/render-functions.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
