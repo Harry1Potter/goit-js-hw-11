92f10f0e119f6272e72509266eccd399e8b09fb1
Max <maks34589@gmail.com>
2024-06-16 17:50:26 +0300

    Your descriptive message here

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,26 @@
+const API_KEY = '44415395-401c6f4260d8ec1c7de985af5';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function fetchImages(query) {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+  });
+
+  const url = `${BASE_URL}?${params}`;
+
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch images');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error('Error fetching images:', error);
+      throw error;
+    });
+}
--- src/main.js
+++ src/main.js
@@ -5,3 +5,49 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+import imageUrl from './img/close.png';
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
+const loader = document.getElementById('loader');
+
+searchForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const searchTerm = searchInput.value.trim();
+
+  if (searchTerm === '') {
+    iziToast.error({
+      title: 'Error',
+      message: 'Please enter a search term.',
+      position: 'topRight',
+      titleColor: '#fff',
+      titleSize: '16px',
+      backgroundColor: 'red',
+      messageColor: 'white',
+      iconUrl: imageUrl,
+      theme: 'dark',
+    });
+    return;
+  }
+
+  loader.style.display = 'block';
+
+  fetchImages(searchTerm)
+    .then(data => {
+      loader.style.display = 'none';
+      displayImages(data.hits);
+      initializeLightbox();
+    })
+    .catch(error => {
+      loader.style.display = 'none';
+    });
+
+  searchForm.reset();
+});
+function initializeLightbox() {
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+  });
+  lightbox.refresh();
+}
