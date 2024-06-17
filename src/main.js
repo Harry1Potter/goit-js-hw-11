873889776b5d0e8cbf23c841a8868e403cea259b
Max <maks34589@gmail.com>
2024-06-17 15:09:04 +0300

    Your commit message

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { fetchImages } from './js/pixabay-api.js';
 import { displayImages } from './js/render-functions.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-
 import imageUrl from './img/close.png';
 
 const searchForm = document.getElementById('search-form');
